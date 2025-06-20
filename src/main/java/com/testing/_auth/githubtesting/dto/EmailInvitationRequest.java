d6a8c863e2e86caf6ea2bd9a94ae3c3d66a2a974
Win Min Thant <25-00313@dat.com>
2025-06-20 16:42:46 +0630

    added calendar servcie and also email

---
+++ src/main/java/com/testing/_auth/githubtesting/dto/EmailInvitationRequest.java
@@ -0,0 +1,16 @@
+/***************************************************************
+ * Author       :	 
+ * Created Date :	
+ * Version      : 	
+ * History  :	
+ * *************************************************************/
+package com.testing._auth.githubtesting.dto;/**
+ * EmailInvitationRequest Class.
+ * <p>
+ * </p>
+ * 
+ * @author
+ */
+
+public class EmailInvitationRequest {
+}
---
+++ src/main/java/com/testing/_auth/githubtesting/service/CalendarEventService.java
@@ -0,0 +1,16 @@
+/***************************************************************
+ * Author       :	 
+ * Created Date :	
+ * Version      : 	
+ * History  :	
+ * *************************************************************/
+package com.testing._auth.githubtesting.service;/**
+ * CalendarEventService Class.
+ * <p>
+ * </p>
+ * 
+ * @author
+ */
+
+public class CalendarEventService {
+}
