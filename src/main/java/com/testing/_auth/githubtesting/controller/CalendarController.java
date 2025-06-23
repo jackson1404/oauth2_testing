b67241f7122d781e303e51da858796825d6138c3
Win Min Thant <127620756+jackson1404@users.noreply.github.com>
2025-06-23 09:39:29 +0630

    Add files via upload

--- src/main/java/com/testing/_auth/githubtesting/controller/CalendarController.java
+++ src/main/java/com/testing/_auth/githubtesting/controller/CalendarController.java
@@ -4,13 +4,40 @@
  * Version      : 	
  * History  :	
  * *************************************************************/
-package com.testing._auth.githubtesting.controller;/**
+package com.testing._auth.githubtesting.controller;
+
+import com.testing._auth.githubtesting.dto.EventRequest;
+import com.testing._auth.githubtesting.service.CalendarEventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+/**
  * CalendarController Class.
  * <p>
  * </p>
- * 
+ *
  * @author
  */
-
+@RestController
+@RequestMapping("/calendars")
 public class CalendarController {
+
+    @Autowired
+    private CalendarEventService calendarEventService;
+
+    @PostMapping("/events")
+    public ResponseEntity<String> createCalendarEvent(
+            @RequestHeader("Authorization") String authHeader,
+            @RequestBody EventRequest eventRequest) {
+        try {
+
+            String accessToken = authHeader.replace("Bearer ", "").trim();
+            String googleEventId = calendarEventService.createGoogleCalendarEvent(accessToken, eventRequest);
+            return ResponseEntity.ok("Event created in Google Calendar with ID: " + googleEventId);
+
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Error: " + e.getMessage());
+        }
+    }
 }
