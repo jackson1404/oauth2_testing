be97f5d502fc6a91f6d5baca5381efacab678745
Win Min Thant <127620756+jackson1404@users.noreply.github.com>
2025-06-23 09:44:11 +0630

    Add files via upload

--- src/main/java/com/testing/_auth/githubtesting/dto/EmailInvitationRequest.java
+++ src/main/java/com/testing/_auth/githubtesting/dto/EmailInvitationRequest.java
@@ -4,13 +4,62 @@
  * Version      : 	
  * History  :	
  * *************************************************************/
-package com.testing._auth.githubtesting.dto;/**
+package com.testing._auth.githubtesting.dto;
+
+import java.util.List;
+
+/**
  * EmailInvitationRequest Class.
  * <p>
  * </p>
- * 
+ *
  * @author
  */
 
 public class EmailInvitationRequest {
+        private String eventSummary;
+        private String startDateTime;
+        private String endDateTime;
+        private String timeZone;
+        private List<String> attendeeEmails;
+
+        public String getEventSummary() {
+                return eventSummary;
+        }
+
+        public void setEventSummary(String eventSummary) {
+                this.eventSummary = eventSummary;
+        }
+
+        public String getStartDateTime() {
+                return startDateTime;
+        }
+
+        public void setStartDateTime(String startDateTime) {
+                this.startDateTime = startDateTime;
+        }
+
+        public String getEndDateTime() {
+                return endDateTime;
+        }
+
+        public void setEndDateTime(String endDateTime) {
+                this.endDateTime = endDateTime;
+        }
+
+        public String getTimeZone() {
+                return timeZone;
+        }
+
+        public void setTimeZone(String timeZone) {
+                this.timeZone = timeZone;
+        }
+
+        public List<String> getAttendeeEmails() {
+                return attendeeEmails;
+        }
+
+        public void setAttendeeEmails(List<String> attendeeEmails) {
+                this.attendeeEmails = attendeeEmails;
+        }
 }
