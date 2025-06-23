d90a314433396d28b6fd92781ca897c299e12d28
Win Min Thant <127620756+jackson1404@users.noreply.github.com>
2025-06-23 09:45:09 +0630

    Update EmailService.java

--- src/main/java/com/testing/_auth/githubtesting/service/EmailService.java
+++ src/main/java/com/testing/_auth/githubtesting/service/EmailService.java
@@ -4,13 +4,35 @@
  * Version      : 	
  * History  :	
  * *************************************************************/
-package com.testing._auth.githubtesting.service;/**
+package com.testing._auth.githubtesting.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+/**
  * EmailService Class.
  * <p>
  * </p>
- * 
+ *
  * @author
  */
 
+@Service
 public class EmailService {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    public void sendSimpleEmail(String toEmail, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("your-email@gmail.com");
+        message.setTo(toEmail);
+        message.setSubject(subject);
+        message.setText(body);
+
+        javaMailSender.send(message);
+    }
+
 }
